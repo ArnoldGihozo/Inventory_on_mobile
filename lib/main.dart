edb7388ce3c6380d16969ba1761be0ab80827582
ArnoldGihozo <gihozo@ualberta.ca>
2021-01-29 20:50:05 -0700

    Updated main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,77 +1,29 @@
+/**
+ * Main.dart
+ * 
+ * Execution of the program will start here. This page will not be 
+ * displayed in any of the application since main.dart redirects
+ * the app to our DashBoardUi page as the home page.
+ */
 import 'package:flutter/material.dart';
 import 'package:inventory_on_mobile/screens/dashboard.dart';
-import 'package:inventory_on_mobile/services/myBottomNavBar.dart';
 
 void main() {
   runApp(MyApp());
-  
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  // Root of application
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Smart Inventory',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: DashboardUI(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-  
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ),
-      bottomNavigationBar: MyBottomNaBar(), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
